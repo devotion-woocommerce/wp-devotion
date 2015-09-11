9040427706f578b3a99762a52fc0547c4202d47c
Ott Tooming <OttTooming@gmail.com>
2015-09-11 23:17:18 +0300

    Add WooCommerce compatibility

--- functions.php
+++ functions.php
@@ -53,8 +53,6 @@ function devotion_setup() {
 	 */
 	add_theme_support( 'html5', array(
 		'search-form',
-		'comment-form',
-		'comment-list',
 		'gallery',
 		'caption',
 	) );
@@ -120,9 +118,6 @@ function devotion_scripts() {
 
 	wp_enqueue_script( 'devotion-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'devotion_scripts' );
 
@@ -150,3 +145,13 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Load WooCommerce compatibility file.
+ */
+require get_template_directory() . '/inc/woocommerce.php';
+
+/**
+ * Remove WooCommerce stylesheet file.
+ */
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
---
+++ inc/woocommerce.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *  Woocommerce
+ *
+ * 
+ *
+ * @package devotion
+ */
+add_theme_support( 'woocommerce' );
