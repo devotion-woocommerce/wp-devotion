efd1643ab221a5e7256892c0450407b930bc3796
Ott Tooming <OttTooming@gmail.com>
2015-09-16 22:50:10 +0300

    Add gulp deployment flow
    For deployment rename secrets_template.json -> secrets.json
    Never push sensitive information to public repositories

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+/* File: gulpfile.js */
+
+// grab our gulp packages
+var gulp  = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    ftp = require( 'vinyl-ftp' ),
+    secrets = require('./secrets.json');
+
+gulp.task( 'deploy', function () {
+
+  gulp.src('./sass/style.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./'));
+
+    var conn = ftp.create( {
+        host:     secrets.servers.development.serverhost,
+        user:     secrets.servers.development.username,
+        password: secrets.servers.development.password,
+        parallel: 10,
+        log:      gutil.log
+    } );
+
+    var globs = [
+        './inc/**',
+        './js/**',
+        './languages/**',
+        './layouts/**',
+        './sass/**',
+        './template-parts/**',
+        '*.php',
+        '*.css',
+        '*.png'
+    ];
+
+    // using base = '.' will transfer everything to /public_html correctly
+    // turn off buffering in gulp.src for best performance
+
+    return gulp.src( globs, { base: '.', buffer: false } )
+        .pipe( conn.newer( secrets.servers.development.remotepath ) ) // only upload newer files
+        .pipe( conn.dest( secrets.servers.development.remotepath ) );
+
+} );
+
+gulp.task('default', function() {
+  gulp.watch('./**', ['deploy']);
+});
