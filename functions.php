fefdef74038cc54aa9037e28d2c737f626d7105e
Ott Tooming <OttTooming@gmail.com>
2015-09-20 18:35:40 +0300

    Add basic product loop item styling
    Add main scss mixins, variables

--- functions.php
+++ functions.php
@@ -199,3 +199,12 @@ function woocommerce_result_count() {
  * Remove "Sort By" from the Woocommerce Shop Page
  */
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+
+/**
+ * Add overlay shadow element before the end of the shop item loop
+ */
+function action_woocommerce_after_shop_loop_item_title( $woocommerce_after_shop_loop_item )
+{
+  echo '<div class="overlay-shadow"></div>';
+}
+add_action( 'woocommerce_after_shop_loop_item', 'action_woocommerce_after_shop_loop_item_title', 10, 2 );
