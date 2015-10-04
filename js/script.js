f7a6fe915d0ad9a2755bd58e48dd34ede717eb0e
Ott Tooming <OttTooming@gmail.com>
2015-10-04 14:58:36 +0300

    Add basic lightbox

--- js/script.js
+++ js/script.js
@@ -29,13 +29,31 @@
   });
 
   if ($('body').hasClass('single-product')) {
+
+    // Switch main preview img based on thumbnails
     $('.thumbnails a').click(function(event) {
       var sourceThumb = $(this).attr('href');
       $(this).closest('.images').find('.woocommerce-main-image').attr('href', sourceThumb);
       $(this).closest('.images').find('.wp-post-image').attr('src', sourceThumb);
       event.preventDefault();
     });
-  }
 
+    // Create overlay lightbox
+    $('.woocommerce-main-image').click(function(event) {
+      $('.images').clone().appendTo('body').addClass('clone product-lightbox');
+      $('body').addClass('lightbox-open');
+
+      var lightbox = $(this).closest('body').find('.product-lightbox');
+
+      $(lightbox).append('<div class="btn-close lightbox-btn-close"></div>');
+      event.preventDefault();
+    });
+
+    // Close lightbox
+    $('body').on('touchend click', '.lightbox-btn-close', function(event) {
+      $(this).closest('.product-lightbox').remove();
+      $('body').removeClass('lightbox-open');
+    })
+  }
 
 } )( jQuery );
