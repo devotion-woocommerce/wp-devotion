de5653bd5ec1d17acd901bb977c7971cf3e3a86a
Ott Tooming <OttTooming@gmail.com>
2015-10-05 22:15:41 +0300

    Remove unused scripts

--- functions.php
+++ functions.php
@@ -132,10 +132,6 @@ add_action( 'widgets_init', 'devotion_widgets_init' );
 function devotion_scripts() {
 	wp_enqueue_style( 'devotion-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'devotion-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'devotion-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	wp_enqueue_script( 'devotion-script', get_template_directory_uri() . '/js/script.js', array(), '20150927', true );
 }
 add_action( 'wp_enqueue_scripts', 'devotion_scripts' );
