a08ba577d370d8a989bb16874f385d6c077d796f
Ott Tooming <OttTooming@gmail.com>
2015-10-12 03:02:26 +0300

    Improve sidebar and search visuals
    Add inline svg search icon

--- functions.php
+++ functions.php
@@ -306,7 +306,11 @@ add_filter( 'woocommerce_product_related_posts_relate_by_tag', function() {
 add_filter( 'get_product_search_form' , 'woo_custom_product_searchform' );
 function woo_custom_product_searchform( $form ) {
 
-	$form = '<div class="toggle-search"></div>
+	$form = '<div class="toggle-search">
+	<svg class="btn-search" width="18px" height="18px" viewBox="0 0 18 18"  xmlns="http://www.w3.org/2000/svg">
+		<path d="M13,11 L12,11 L12,11 C13,10 13,8 13,7 C13,3 10,0 7,0 C3,0 0,3 0,7 C0,10 3,13 7,13 C8,13 10,13 11,12 L11,12 L11,13 L16,18 L18,16 L13,11 ZM7,11 C4,11 2,9 2,7 C2,4 4,2 7,2 C9,2 11,4 11,7 C11,9 9,11 7,11 Z"></path>
+	</svg>
+	</div>
 	<form role="search" method="get" id="searchform" class="searchform" action="' . esc_url( home_url( '/'  ) ) . '">
 		<div>
 			<label class="screen-reader-text" for="s">' . __( 'Search for:', 'woocommerce' ) . '</label>
