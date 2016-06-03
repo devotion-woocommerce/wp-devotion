97a04c0041864cb31798013a6aa1701534d34e34
Ott Tooming <OttTooming@gmail.com>
2016-06-03 16:27:00 +0300

    Remove unneeded WP registered actions

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@ if ( ! function_exists( 'devotion_setup' ) ) :
  * as indicating support for post thumbnails.
  */
 function devotion_setup() {
+
 	/*
 	 * Make theme available for translation.
 	 * Translations can be filed in the /languages/ directory.
@@ -27,9 +28,6 @@ function devotion_setup() {
 	 * to change 'devotion' to the name of your theme in all the template files.
 	 */
 	load_theme_textdomain( 'devotion', get_template_directory() . '/languages' );
-
-	// Add default posts RSS feed link to head.
-	add_theme_support( 'automatic-feed-links' );
 
 	/*
 	 * Let WordPress manage the document title.
@@ -73,11 +71,6 @@ function devotion_setup() {
 		'link',
 	) );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'devotion_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
 }
 endif; // devotion_setup
 add_action( 'after_setup_theme', 'devotion_setup' );
@@ -169,10 +162,18 @@ function devotion_used_scripts_styles() {
 function devotion_dequeue_scripts_styles(){
 	wp_deregister_style('wpr_giftcards_css');
 
-	wp_deregister_script('jquery');
-	wp_deregister_script('woocommerce');
-	wp_deregister_script('wc-cart-fragments');
-	wp_deregister_script('wpr_giftcards_js ');
+	wp_deregister_script(	'jquery'	);
+	wp_deregister_script(	'woocommerce'	);
+	wp_deregister_script(	'wc-cart-fragments'	);
+	wp_deregister_script(	'wpr_giftcards_js '	);
+	wp_deregister_script( 'prettyPhoto' );
+	wp_deregister_script( 'prettyPhoto-init' );
+	wp_deregister_script( 'woocommerce_prettyPhoto_css' );
+
+	wp_dequeue_script( 'prettyPhoto' );
+	wp_dequeue_script( 'prettyPhoto-init' );
+	wp_deregister_style( 'woocommerce_prettyPhoto_css' );
+
 }
 add_action( 'wp_print_styles', 'devotion_dequeue_scripts_styles', 100 );
 
@@ -191,6 +192,14 @@ remove_action( 'wp_head', 'wp_generator'); // Display the XHTML generator that i
 remove_action(' wp_head', 'feed_links_extra', 3); // Remove category feeds
 remove_action(' wp_head', 'feed_links', 2); // Remove Post and Comment
 
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
 /**
  * Enqueue scripts and styles.
  */
@@ -199,6 +208,7 @@ function devotion_scripts() {
 	wp_enqueue_style( 'devotion-style', get_stylesheet_directory_uri() . '/dist/styles/style.min.css', array(), '1', 'all' );
 
 	wp_enqueue_script( 'devotion-script', get_template_directory_uri() . '/dist/scripts/script.min.js', array(), '1', true );
+	
 }
 add_action( 'wp_enqueue_scripts', 'devotion_scripts' );
 
